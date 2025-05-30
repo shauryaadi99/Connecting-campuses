737359e2790c61a6f60d3872490846590ef517b4
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2025-05-30 21:35:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://connecting-campuses.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
