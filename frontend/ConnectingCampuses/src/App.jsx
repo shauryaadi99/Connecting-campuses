9daf9a5ce4b6e095cdb254b5bb04b738ff9e7909
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2025-05-31 20:23:00 +0530

    Update App.jsx

--- frontend/ConnectingCampuses/src/App.jsx
+++ frontend/ConnectingCampuses/src/App.jsx
@@ -19,34 +19,36 @@ import NewsroomDashboard from "./UserNewsroomList";
 import VerifyEmailPage from "./verify-email";
 import ForgotPassword from "./ForgotPassword";
 import ResetPassword from "./ResetPassword";
+import ImagePickerTest from "./ImagePickerTest";
+
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route path="/test" element={<h1>Test Page</h1>} />
-        <Route path="/attendance" element={<AttendenceManager />} />
-        <Route path="/sellBuy" element={<SellBuyPage />} />
-        <Route path="/newsroom" element={<NewsroomListing />} />
-        <Route path="/lostfound" element={<LostAndFoundListing />} />
-        <Route path="/carpooling" element={<CarpoolingListing />} />
-        <Route path="/signup" element={<SignupFormDemo />} />
-        <Route path="/profile" element={<ProfileCard />} />
-        <Route path="/update-profile" element={<UpdateProfileForm />} />
-        <Route path="/deleteallAttendance" element={<ClearAttendanceButton />} />
-        <Route path="/mynewsroomlistings" element={<NewsroomDashboard />} />
-        <Route path="/verify-email" element={<VerifyEmailPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-
-
-      </Routes>
-      <Footer />
-    </>
-  );
+ return (
+  <>
+    <Navbar />
+    <ScrollToTop />
+    <Routes>
+      <Route path="/" element={<MainLayout />} />
+      <Route path="/test" element={<h1>Test Page</h1>} />
+      <Route path="/attendance" element={<AttendenceManager />} />
+      <Route path="/sellBuy" element={<SellBuyPage />} />
+      <Route path="/newsroom" element={<NewsroomListing />} />
+      <Route path="/lostfound" element={<LostAndFoundListing />} />
+      <Route path="/carpooling" element={<CarpoolingListing />} />
+      <Route path="/signup" element={<SignupFormDemo />} />
+      <Route path="/profile" element={<ProfileCard />} />
+      <Route path="/update-profile" element={<UpdateProfileForm />} />
+      <Route path="/deleteallAttendance" element={<ClearAttendanceButton />} />
+      <Route path="/mynewsroomlistings" element={<NewsroomDashboard />} />
+      <Route path="/verify-email" element={<VerifyEmailPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      {/* ✅ New Image Picker Testing Route */}
+      <Route path="/test-image-picker" element={<ImagePickerTest />} />
+    </Routes>
+    <Footer />
+  </>
+);
 };
 
 export default App;
